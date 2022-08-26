50943a843d74a1c8566042a0455112ceb0615eac
Rafael V Oliveira <66887480+rvoliveirafurg@users.noreply.github.com>
2022-08-26 12:10:35 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,7 +35,7 @@ cat <<EOF >$HOME/.docker/config.json
 }
 EOF
 
-
+cat $HOME/.docker/config.json
 
 export CONTEXT="$CONTEXT_PATH"
 
@@ -50,8 +50,8 @@ echo "Destination: $DESTINATION"
 export ARGS="--push $DESTINATION $DOCKERFILE  --allow security.insecure $CONTEXT"
 echo "Args: $ARGS"
 
-echo "Building image"
-buildx create --use --name insecure-builder --buildkitd-flags '--allow-insecure-entitlement security.insecure'
+#echo "Building image"
+#buildx create --use --name insecure-builder --buildkitd-flags '--allow-insecure-entitlement security.insecure'
 
 buildx build $ARGS || exit 1
 
