0c84e97d87187217916d102ddeb2d64d3859b4aa
Rafael V Oliveira <66887480+rvoliveirafurg@users.noreply.github.com>
2022-08-29 13:59:45 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -73,7 +73,9 @@ echo "Building image"
 
 #buildx create --use --name insecure-builder --buildkitd-flags "--allow-insecure-entitlement security.insecure"
 
-buildx create --use --config /etc/buildkit-config.toml
+buildx create --use --config /etc/buildkit-config.toml --name mybuild
+
+buildx create --append --name mybuild
 
 buildx inspect --bootstrap
 
